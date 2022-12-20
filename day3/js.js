92a23680a8a88c380f870157be0953f2ff2d95cf
Marwa996 <marwamoneim221@gmail.com>
2022-12-20 23:12:05 +0200

    day 3

--- day3/js.js
+++ day3/js.js
@@ -6,14 +6,11 @@ var clickbutton = document.getElementById('home');
 var arr=[clickbutton,resumeBtn, workBtn, contactBtn];
 
 clickbutton.addEventListener('click', function(){
-    // console.log(this.getAttribute('aria-expanded'))
-    // var closed =  this.getAttribute('aria-expanded');
+
     for(let i=0;i<arr.length;i++){
-        // console.log(arr[i].getAttribute('aria-expanded'),i)
-        // if (arr[i].classList('show')) {
+        if (arr[i].classList('show')) {
             console.log(arr[i].classList());
             arr[i].classList.remove("show");
             arr[i].classList.add("collapsed")
             // console.log(arr[i].classList());
-            // console.log(this.getAttribute('aria-expanded'),i)
-        }});
+        }}});
